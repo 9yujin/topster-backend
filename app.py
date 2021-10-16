a8df39a52b47ed65d9812b0d62dd93d825ae63cd
“9yujin” <david0218@naver.com>
2021-10-16 19:22:54 +0900

    211016

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from re import search
-from flask import Flask, request
+from flask import Flask, jsonify, request
 import pprint
 import sys
 import spotipy
@@ -7,7 +7,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/albums/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 cid = "79b0fc127f5a49978ea0045844685dcf"
 secret = "05cb5b72719d435f8b02df73658b44a6"
@@ -20,28 +20,32 @@ else:
 
 res = sp.search(search_str, limit="20",type='album', market='KR') """
 
-@app.route('/albums')
+@app.route('/api/albums')
 def get_arts():
     search_str = request.args.get('search')
     print(search_str)
 
+    test = sp.search(search_str, limit="50",type='album', market='KR')['albums']['items']
+    pprint.pprint(test)
     response = sp.search(search_str, limit="50",type='album', market='KR')['albums']['items']
     images=[]
     titles=[]
     artists=[]
+    ids=[]
     for i in response:
         images.append(i['images'][0]['url'])
         titles.append(i['name'])
         artists.append(i['artists'][0]['name'])
+        ids.append(i['id'])
     result = {}
     result_tmp = []
     for i in range(len(images)):
-        result_tmp.append({"image": images[i], "title": titles[i], "artist": artists[i]})
+        result_tmp.append({"image": images[i], "title": titles[i], "artist": artists[i], "id": ids[i]})
     result["res"] = {"albums": result_tmp}
-    pprint.pprint(result)
+    """ pprint.pprint(result) """
     return result
    
 
 if __name__ == '__main__':  
-   app.run(debug=True)
+   app.run(host='0.0.0.0', debug=True)
  
